b1376ff32941c7d075c4de92b60958538da3c29e
TsuuGuu <ilhamm711@gmail.com>
2022-03-01 15:56:07 +0700

    first commit

---
+++ lib/screen/signin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Signin extends StatelessWidget {
+  const Signin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("So.Je", Googlefont.),
+      ),
+    );
+  }
+}
